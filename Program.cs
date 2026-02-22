f5ac8e5a76b42c535cc7e5631cf30e9cea20f212
Mujab Nasraldeen <mujab-it@hotmail.com>
2026-02-23 01:54:46 +0300

    Add 𝗦𝗰𝗮𝗹𝗮𝗿 for API Documentation

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using CourseFirstApp.Data;
 using CourseFirstApp.IServices;
 using CourseFirstApp.Services;
 using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -25,6 +26,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
